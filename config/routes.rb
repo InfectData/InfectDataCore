a3d246024b420771870c56cd57e64bd60b3fdd78
Amina <50828174+AminaDr@users.noreply.github.com>
2020-12-04 09:57:03 +0100

    Anpassungen Devise
    sign in, out, up
    Root to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-get '/' => 'content#homepage'
+  get '/' => 'content#homepage'
+  root to: "content#homepage"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
