c0ec09f1b64f3a3373e6e1127feb50e47d9ebb26
Amina <50828174+AminaDr@users.noreply.github.com>
2020-12-08 15:02:06 +0100

    Schutz der Startseite durch Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+before_action :authenticate_user!
+#  def after_sign_in_path_for(resource)
+#    @user = User.find (current_user)
+#    if user.role = 'admin'
+#    return "content#admin"
+#   end
+# end
+
+#def after_sign_in_path_for(resource)
+#  stored_location_for(resource) || "content#admin"
+#end
+
 end
