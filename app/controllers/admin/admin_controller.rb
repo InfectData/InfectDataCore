54b849878c0498513c4a1449e0ce289992c69cc2
Amina <50828174+AminaDr@users.noreply.github.com>
2020-12-09 17:58:12 +0100

    Modul für den Admin-Bereich

---
+++ app/controllers/admin/admin_controller.rb
@@ -0,0 +1,11 @@
+class Admin::AdminController < ApplicationController
+
+before_action :authenticate_user!
+before_action :current_user_admin?
+
+def current_user_admin?
+return if current_user.role == 'admin'
+redirect_to root_path
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get '/' => 'content#homepage'
   root to: "content#homepage"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    scope '/admin', module: :admin do
+    resources :users
+    end
 end
