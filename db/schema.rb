179e57a0da7dc5108e8dd19ae6243e0279a4c947
Amina <50828174+AminaDr@users.noreply.github.com>
2020-12-14 13:04:48 +0100

    Migration in die DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_102840) do
+ActiveRecord::Schema.define(version: 2020_12_14_115254) do
+
+  create_table "cases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "gender"
+    t.date "birthdate"
+    t.string "place_of_residence"
+    t.string "diagnosis"
+    t.bigint "user_id", null: false
+    t.datetime "confirmed_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cases_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_12_14_102840) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cases", "users"
 end
