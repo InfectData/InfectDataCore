cc6390ece4b394ce48d1e55723a75f5094806475
Amina <50828174+AminaDr@users.noreply.github.com>
2020-12-18 17:20:36 +0100

    Migration
    Um Diagnosen mit Fällen zu verknüpfen

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_18_160012) do
+ActiveRecord::Schema.define(version: 2020_12_18_160615) do
 
   create_table "cases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "first_name"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2020_12_18_160012) do
     t.datetime "confirmed_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "diagnosis_id", null: false
+    t.index ["diagnosis_id"], name: "index_cases_on_diagnosis_id"
     t.index ["user_id"], name: "index_cases_on_user_id"
   end
 
@@ -47,5 +49,6 @@ ActiveRecord::Schema.define(version: 2020_12_18_160012) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cases", "diagnoses"
   add_foreign_key "cases", "users"
 end
