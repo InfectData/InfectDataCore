b310b444db021d467759d788f32b1a2559345225
Amina <50828174+AminaDr@users.noreply.github.com>
2021-01-05 14:03:52 +0100

    Check Confirmed/Unconfirmed

--- app/models/case.rb
+++ app/models/case.rb
@@ -2,4 +2,8 @@ class Case < ApplicationRecord
   belongs_to :user
   belongs_to :diagnosis
   belongs_to :district
+
+  scope :unconfirmed, -> { where(confirmed_at: nil) }
+  scope :confirmed, -> { where.not(confirmed_at: nil) }
+  
 end
