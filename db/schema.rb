3d046d9437f6c8fd9d4cd558f7efd6c8ce0a1680
Amina <50828174+AminaDr@users.noreply.github.com>
2021-01-27 10:58:23 +0100

    Model for State

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_125842) do
+ActiveRecord::Schema.define(version: 2021_01_27_095447) do
 
   create_table "cases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "first_name"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2021_01_07_125842) do
     t.string "name"
     t.string "place"
     t.integer "postal_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "states", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "titel"
+    t.string "abbr"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
