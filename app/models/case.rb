f249ef65bd70800d83b37ba2da7455dd5244e353
Amina <50828174+AminaDr@users.noreply.github.com>
2021-02-10 11:17:31 +0100

    Update case.rb

--- app/models/case.rb
+++ app/models/case.rb
@@ -2,7 +2,7 @@ class Case < ApplicationRecord
   belongs_to :user
   belongs_to :diagnosis
   belongs_to :district
-  belongs_to :report
+  belongs_to :report, optional: true
 
   scope :unconfirmed, -> { where(confirmed_at: nil) }
   scope :confirmed, -> { where.not(confirmed_at: nil) }
