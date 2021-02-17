16cf2caad24eeed40af4b53ccccaeb0e5834de47
Amina <50828174+AminaDr@users.noreply.github.com>
2021-02-17 22:14:21 +0100

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,32 @@
 Rails.application.routes.draw do
 
+#_____________District________________________
   scope '/ga', module: :ga do
     resources :districts
   end
-
+  resources :districts
+#_____________Report___________________________
   scope '/ls', module: :ls do
     resources :reports
   end
-
+  resources :reports
+#_____________Diagnoses________________________
   scope '/rki', module: :rki do
     resources :diagnoses, only: :index do
       get 'cases', on: :member
     end
   end
-
-  resources :reports
-  resources :districts
   resources :diagnoses
-
+#_____________Cases____________________________
   resources :cases do
     post 'confirm', on: :member
   end
-
-  devise_for :users
-
-  root to: "content#homepage"
-
+#_____________Users____________________________
   scope '/admin', module: :admin do
     resources :users
   end
+#______________________________________________
+  devise_for :users
+  root to: "content#homepage"
 
 end
