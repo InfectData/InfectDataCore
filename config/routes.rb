62c0e6420b600eb7b2a0f9e6254f6d72d47c4120
Amina <50828174+AminaDr@users.noreply.github.com>
2021-03-17 14:15:40 +0100

    Routes + RKI

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,31 @@
 Rails.application.routes.draw do
 
-#_____________District________________________
+  #_____________District________________________
   scope '/ga', module: :ga do
     resources :districts
   end
   resources :districts
-#_____________Report___________________________
+  #_____________Report___________________________
   scope '/ls', module: :ls do
     resources :reports
   end
   resources :reports
-#_____________Diagnoses________________________
+  #_____________Diagnoses________________________
   scope '/rki', module: :rki do
-    resources :diagnoses, only: :index do
+    resources :diagnoses, as: "rki_diagnoses", only: :index do
       get 'cases', on: :member
     end
   end
   resources :diagnoses
-#_____________Cases____________________________
+  #_____________Cases____________________________
   resources :cases do
     post 'confirm', on: :member
   end
-#_____________Users____________________________
+  #_____________Users____________________________
   scope '/admin', module: :admin do
     resources :users
   end
-#______________________________________________
+  #______________________________________________
   devise_for :users
   root to: "content#homepage"
 
